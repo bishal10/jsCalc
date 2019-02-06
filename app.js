fd040a0a0c4c9a9b668316a75442530c38be7d68
bishal10 <rautbishal10@gmail.com>
2019-02-06 14:21:47 +0545

    invalid operation restricted and simple UIchange

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-function displayNumber(n1){
-calcform.txt1.value = calcform.txt1.value+n1;
+let repeatSign = [];
+// n1 contain actual value and n2 contain indicator of string "norepeatation"
+function displayNumber(n1, n2) {
+  // displaying valid operation in text area of input field
+  calcform.txt1.value = calcform.txt1.value + n1;
+
+  var newLength = repeatSign.push(n2);
+  repeatSign.forEach(function(item, index, array) {
+    // checking for same sign at once like ++ or ** for invalid operation
+    if (repeatSign[index] && repeatSign[index - 1] === "norepeatation") {
+      calcform.txt1.value = "Invalid use of sign";
+      repeatSign.fill(0);
+      var delayInMilliseconds = 3000;
+
+      setTimeout(function() {
+        calcform.txt1.value = "";
+      }, delayInMilliseconds);
+    }
+  });
 }
 
-function clearDisplay(){
-    calcform.txt1.value = '';
+function clearDisplay() {
+  calcform.txt1.value = "";
+  repeatSign.fill(0);
 }
